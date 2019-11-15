878f67fb16c2c4f892b19f9c219553ccef6feb53
Ryan Challen <ryanchallen@outlook.com>
2019-11-15 16:00:49 +1000

    Defined some variables to help with game play and articulated the opening sequence, complete with coin toss to determine first mover

--- run_app.rb
+++ run_app.rb
@@ -1,3 +1,14 @@
+player_1_health = rand(1..50)
+player_2_health = rand(1..50)
+
+hit_chance = rand(5..45)
+
+starting_room1 = rand(1..6)
+
+starting_room2 = rand(1..6)
+
+coin_toss = rand(1..2)
+
 puts "Welcome to RUN!"
 
 sleep(1)
@@ -24,5 +35,26 @@ puts "."
 sleep(1)
 puts "BOO!"
 sleep(1)
-puts "Got ya! Ok... let's go!"
+puts "Got ya!"
+sleep(1)
+puts "Ok, let's go!"
+sleep(1)
+
+puts "Let's see who moves first. Flipping a cursed coin... if it's Heads it's you, #{player_1}, and Tails it's you, #{player_2}!"
+
+coin_toss
+
+if coin_toss == 1
+    puts "It's Heads! You're up, #{player_1}. Choose a room number to move to out of the 6 that isn't #{starting_room1}:  "
+    room_choice1 = gets.chomp.to_i
+else 
+    puts "It's Tails! You're up, #{player_2}. Choose a room number to move to out of the 6 that isn't #{starting_room2}:"
+    room_choice2 = gets.chomp.to_i
+end
+
+if room_choice1 == room_choice2
+    FIGHT
+else
+    cycle to next player turn
+end
 
