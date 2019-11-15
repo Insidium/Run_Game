9f2e1329dce89fa9708a39c5ff66756b8da32c33
Ryan Challen <ryanchallen@outlook.com>
2019-11-15 15:16:00 +1000

    Started coding the entry to the game experience. Also commencing the development plan document.

---
+++ SoftwareDevPlan.md
@@ -0,0 +1,18 @@
+# Software Development Plan
+
+### Statement of Purpose and Scope
+
+#### What is 'RUN!'?
+
+
+#### The purpose 
+
+
+
+#### Target audience
+
+
+
+#### How to play 
+
+
---
+++ run_app.rb
@@ -0,0 +1,28 @@
+puts "Welcome to RUN!"
+
+sleep(1)
+
+puts "Player 1, you are the brave 'Survivor'. Enter your name: "
+
+player_1 = gets.chomp.capitalize.to_s
+
+puts "Player 2, you are the brutal 'Terror'. Enter your name: "
+
+player_2 = gets.chomp.capitalize.to_s
+
+puts "Ok, #{player_1} and #{player_2}, let's descend into madness... or something spooky like that..."
+
+sleep(1)
+
+puts "."
+sleep(1)
+puts "."
+sleep(1)
+puts "."
+sleep(1)
+puts "."
+sleep(1)
+puts "BOO!"
+sleep(1)
+puts "Got ya! Ok... let's go!"
+
