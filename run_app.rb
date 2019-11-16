c0ad5a7097720725a026038c5365eb9ae9fd8ee4
Ryan Challen <ryanchallen@outlook.com>
2019-11-16 13:45:19 +1000

    Fixed an error with scope of game choices. Revised logic for end of game.

--- run_app.rb
+++ run_app.rb
@@ -1,27 +1,35 @@
-player_health = 50
-monster_health = 50
-
-current_room = rand(1..6)
-monster_room = nil
-
-game_over = false
-
-force_to_same_room = false
 
 quit = false
 
-puts "Welcome to RUN!"
+def flee
+    puts "You flee towards another room! Which one do you choose?"
+    current_room = gets.chomp.to_i - 1
+    force_to_same_room = false
+    return current_room, force_to_same_room
+end
 
-# sleep(1)
+while quit == false
 
-puts "Player, you are the brave 'Survivor'. Enter your name: "
+    player_health = 50
+    monster_health = 50
 
-player_name = gets.chomp.capitalize.to_s
+    current_room = rand(1..6)
+    monster_room = nil
 
-# puts "Player 2, you are the brutal 'Terror'. Enter your name: "
+    force_to_same_room = false
+
+    puts "Welcome to RUN!"
+
+    # sleep(1)
+
+    puts "Player, you are the brave 'Survivor'. Enter your name: "
+
+    player_name = gets.chomp.capitalize.to_s
+
+    # puts "Player 2, you are the brutal 'Terror'. Enter your name: "
 
 
-puts "Ok, #{player_name}, let's descend into madness... or something spooky like that..."
+    puts "Ok, #{player_name}, let's descend into madness... or something spooky like that..."
 
 # sleep(1)
 
@@ -42,8 +50,10 @@ puts "Ok, #{player_name}, let's descend into madness... or something spooky like
 
 # puts "Let's see who moves first. Flipping a cursed coin... if it's Heads it's you, #{player_1}, and Tails it's you, #{player_2}!"
 
-puts "It's Heads! You're up, #{player_name}. Choose a room number to move to (out of the 6) that isn't #{current_room}:  "
-current_room = gets.chomp.to_i - 1
+# puts "It's Heads! You're up, #{player_name}. Choose a room number to move to (out of the 6) that isn't #{current_room}:  "
+
+    puts "Brave #{player_name}, choose a room number to move to (out of the 6): "
+    current_room = gets.chomp.to_i - 1
 
 # if room_choice1 == room_choice2
 #     puts "*cue the dramatic music*" 
@@ -53,25 +63,13 @@ current_room = gets.chomp.to_i - 1
 #     sleep(1)
 #     puts "The journey continues..."
 # end
-
-def flee
-    puts "You flee towards another room! Which one do you choose?"
-    current_room = gets.chomp.to_i - 1
-    force_to_same_room = false
-    return current_room, force_to_same_room
-end
-
-while quit == false
-
     
-    until game_over == true
+    until monster_health <= 0 || player_health <= 0
 
         monster_room = force_to_same_room ? current_room : rand(0..5)
 
-        puts "New loop"
-
         if monster_room == current_room
-            puts "Monster here. Choose move: 1 attack 2 flee"
+            puts "Monster here. Choose move: 1 - Attack OR 2 - Flee"
             choice = gets.chomp
 
             case choice 
@@ -80,16 +78,16 @@ while quit == false
                 puts "You lash out wildly at monster and hit damage them #{hit = rand(5..45)} health points!"
                 monster_health -= hit
                 if monster_health <= 0
-                    game_over = true
+                    
                     break
                 end
                 puts "The monster hits you back for #{hit = rand(5..45)}"
                 player_health -= hit
                 if player_health <= 0
-                    game_over = true
+
                     break
                 end
-                puts "game over is #{game_over}"
+                
                 puts "Try to flee? Of course you do you don't want to die."
                 current_room, force_to_same_room = flee()
             when "2"
@@ -109,8 +107,9 @@ while quit == false
 
             when "2"
                 puts "You look around..."
-                force_to_same_room == true
-                # monster_room = current_room
+                force_to_same_room = true
+                puts "monster health is #{monster_health}"
+                puts "player health is #{player_health}"
 
             when "3"
                 puts "You nervously choose another room! Which one do you choose?"
@@ -120,14 +119,13 @@ while quit == false
             end
         end
     end
+end
 
-    puts "Monter_health: #{monster_health}"
+    puts "Monster_health: #{monster_health}"
     puts "Play_health: #{player_health}"
     
     player_win = "The monster shreeks and falls to the floor dead. You did it, #{player_name}! YOU SURVIVED!"
     monster_win = "You fall to the floor fatally wounded. The monster comes in for the kill. You black out from the pain. Game Over, #{player_name}!"
-    
-    # monster_health <= 0 ? (puts player_win) : (puts monster_win)
     
     puts monster_health <= 0 ? player_win : monster_win
     
@@ -137,7 +135,7 @@ while quit == false
     if user_input == "y"
         quit == true
     end
-end
+
 
     #     puts "Select your action #{player_name}: 1 - Attack, 2 - RUN!"
     #     action_choice_1 = gets.chomp.to_i
