6b29494292b0dbf6569547f2ce14d84f5b0b2818
Ryan Challen <ryanchallen@outlook.com>
2019-11-20 13:39:57 +1000

    Beginning to add error handling to inputs. Also implementing gems for style and user experience.

--- run_app.rb
+++ run_app.rb
@@ -1,43 +1,52 @@
+require "colorize"
+require "tty-prompt"
+require "tty-font"
+
+font = TTY::Font.new(:doom)
+pastel = Pastel.new
+prompt = TTY::Prompt.new
+
 class Character
 
     attr_accessor :name, :health, :win_message, :score
+     
+    # @@points = points
 
     def initialize(name="Monster")
-        @health = 50
         @name = name
+        @health = @name == "Monster" ? 75 : 50 #debug
         @score = 0
         @win_message = name == "Monster" ? "You fall to the floor fatally wounded. The monster comes in for the kill. You black out from the pain. Game Over!" : "The monster shreeks and falls to the floor dead. You did it, #{@name}! YOU SURVIVED!"
     end
 
+    # action that adds 10 health points to player if not at max health
     def rest
-        @health += 10
         if @health > 50
             puts "You are already at max health!"
             @health = 50
+        else 
+            @health += 10
+            puts "You regain 10 health points!"
         end
     end
 
+    # action that determines damage to player and monster characters and evaluates if reached 0 health
     def hit(damage)
         @health -= damage
         if @health <= 0
             @health = 0
         end
     end
-end
 
-class File
-    attr_accessor :show, :add
-
-    def initialize (show, add)
-        @show = show
-        @add = add
+    def tally(points)
+        @@points = damage + 25
     end
-
 end
 
-
+# action that allows player to change room number and pre
 def flee
     puts "You flee towards another room! Which one do you choose?"
+    
     current_room = gets.chomp.to_i - 1
     force_to_same_room = false
     return current_room, force_to_same_room
@@ -45,8 +54,8 @@ end
 
 def end_of_game(value, high, user)
     puts user.win_message
-    File.write("recent_games.txt", "#{value == 'win' ? "Winner" : "Loser"}%^&", mode: "a")
-    File.write("high_scores.txt", "#{high}%^&", mode: "a")
+    File.write("recent_games.txt", "#{user.name} - #{value == 'win' ? "Winner!" : "You lost!"}%^&", mode: "a")
+    File.write("high_scores.txt", "#{user.name} - #{user.score}%^&", mode: "a")
 end
 
 quit = false
@@ -54,6 +63,8 @@ quit = false
 replay = true
 
 until replay == false
+    puts pastel.red(font.write("RUN!" , letter_spacing: 4))
+
     puts "Menu:"
     puts "1 - High Scores"
     puts "2 - Recent Results"
@@ -61,13 +72,15 @@ until replay == false
     puts "4 - Quit"
 
     case gets.chomp
-    when "1" #high scores
+    when "1" # display high scores
         puts "High Scores"
-        high_scores = File.read("high_scores.txt")
-    when "2" #recent wins/losses
+        high_scores = File.read("high_scores.txt").split("%^&")
+        p high_scores
+    when "2" # display recent wins/losses
         puts "Recents"
-        recent_games = File.read("recent_games.txt")
-    when "3" #play
+        recent_games = File.read("recent_games.txt").split("%^&")
+        p recent_games
+    when "3" #play the game
         while quit == false
         
                 monster = Character.new()
@@ -109,47 +122,42 @@ until replay == false
         
                 # puts "It's Heads! You're up, #{player_name}. Choose a room number to move to (out of the 6) that isn't #{current_room}:  "
         
-                puts "Choose a room number to move to (out of the 6): "
-                current_room = gets.chomp.to_i - 1
-        
-                # if room_choice1 == room_choice2
-                #     puts "*cue the dramatic music*" 
-                #     sleep(1)
-                #     puts "Oh no! The two worlds collide! FIGHT TO SURVIVE!"
-                # else 
-                #     sleep(1)
-                #     puts "The journey continues..."
-                # end
+                puts "You are in room #{current_room}. Choose another room number to move to (1 - 6): "
+                current_room = gets.chomp.to_i
+                until current_room > 0 and current_room <= 6 do
+                    puts "That is not a valid choice. Please enter a number between 1 and 6"
+                    current_room = gets.chomp.to_i
+                end
                 
                 until monster.health <= 0 || player.health <= 0
         
-                    monster_room = force_to_same_room ? current_room : rand(0..5)
+                    monster_room = force_to_same_room ? current_room : rand(1..6)
         
                     if monster_room == current_room
-                        puts "Monster here. Choose move: 1 - Attack OR 2 - Flee"
+                        puts "Aaah! The Terror is here! Choose your move: 1 - Attack OR 2 - Flee"
                         choice = gets.chomp
         
                         case choice 
                             
                         when "1"
-                            puts "You lash out wildly at monster and hit damage them #{hit = rand(5..45)} health points!"
+                            puts "You lash out wildly at the Terror and wound it #{hit = rand(5..65)} health points!"
                             monster.hit(hit)
                             break if monster.health <= 0
-                            puts "The monster hits you back for #{hit = rand(5..45)}"
+                            puts "The Terror slashes you back for #{hit = rand(5..45)}"
                             player.hit(hit)
                             break if player.health <= 0
                             
-                            puts "The monster's health is #{monster.health}"
-                            puts "Your health is #{player.health}"
+                            puts "The Terror's health is now #{monster.health}"
+                            puts "Your health is now #{player.health}"
                             
-                            puts "Try to flee? Of course you do you don't want to die."
+                            puts "Try to flee? Of course! You do you don't want to DIE!"
                             current_room, force_to_same_room = flee()
                         when "2"
                             current_room, force_to_same_room = flee()
                         end
         
                     else
-                        puts "You enter an empty room. What do you do? 1. Rest, 2. Look around, 3. Move to next room (choose)"
+                        puts "You enter an empty, creepy room. What do you do? 1. Rest for a while, 2. Look around for help, 3. Move to another room"
                         option = gets.chomp
         
                         case option
@@ -179,8 +187,9 @@ until replay == false
                     quit = true
                 end
             end
-    when "4" # quit
-        puts "A terrifying voice growls: 'GET OUT!'"
+    when "4" # quit the application
+        puts pastel.red.bold("A horrifying voice growls: ")
+        puts pastel.red.bold(font.write("GET OUT!"))
         replay = false
     end
 end
